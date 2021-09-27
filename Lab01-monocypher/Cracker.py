fd4b13282987b40e8fa4f251bc4dcd6bb2f69a20
turtlelazy <ishraqmahid@gmail.com>
2021-09-27 19:34:04 -0400

    code organization

--- Lab01-monocypher/Cracker.py
+++ Lab01-monocypher/Cracker.py
@@ -1,16 +1,19 @@
 from sys import argv
+
+#65 and 97 are A and a respectively
+def counters(text):
+    counters = {}
+
+    for letter in range(26):
+        count = text.count(chr(letter + 65)) + text.count(chr(letter + 97))
+        counters[chr(letter+65)] = count
+    #print(counters)
+    return counters
+
+
 input = argv[1]
 file = open(input)
 
 text = file.read()
 
-#print(text)
-
-#65 and 97 are A and a respectively
-
-counters = {}
-
-for letter in range(26):
-    count = text.count(chr(letter + 65)) + text.count(chr(letter + 97))
-    counters[chr(letter+65)] = count
-print(counters)
+counters = counters(text)
