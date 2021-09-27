21f9add790016d43d6e4f6b49efd9d440b62bc6a
turtlelazy <ishraqmahid@gmail.com>
2021-09-27 19:31:57 -0400

    wrote line to count letter frequency

--- Lab01-monocypher/Cracker.py
+++ Lab01-monocypher/Cracker.py
@@ -4,5 +4,13 @@ file = open(input)
 
 text = file.read()
 
-print(text)
+#print(text)
 
+#65 and 97 are A and a respectively
+
+counters = {}
+
+for letter in range(26):
+    count = text.count(chr(letter + 65)) + text.count(chr(letter + 97))
+    counters[chr(letter+65)] = count
+print(counters)
